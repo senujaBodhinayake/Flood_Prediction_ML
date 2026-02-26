c9b272ada1cd9f6681f15d222086bbf4f56c66de
senujaBodhinayake <senujabodhinayake123@gmail.com>
2026-02-26 12:28:42 +0530

    split dataset

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("data/flood.csv")
+
+# Seperate features and target variable
+
+x = df.drop(columns=["FloodProbability"]) # inputs
+y= df["FloodProbability"] # output
+
+# Split the data into training and testing sets
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+
+print("Training rows:",x_train.shape[0])
+print("Testing rows:",x_test.shape[0])
+
+
 
 print("Shape (rows, columns):", df.shape)
 print("\nColumn names:\n", df.columns)
